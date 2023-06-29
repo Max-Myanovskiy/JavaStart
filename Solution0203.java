dcb23093ca765297448c8f0687d006a9b98f1901
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-06-29 21:31:05 +0300

    Create Solution0203

---
+++ Solution0203.java
@@ -0,0 +1,12 @@
+/*
+Последняя цифра числа
+*/
+
+public class Solution0203 {
+
+	public static void main(String[] args) {
+		int number = 546;
+		int lastDigit = number % 10;
+		System.out.println(lastDigit);
+	}
+}
