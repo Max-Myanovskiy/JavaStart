1428b576710878519a37510498905f0e0f2e0410
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-06-29 21:33:34 +0300

    Create Solution0205

---
+++ Solution0205.java
@@ -0,0 +1,20 @@
+/*
+Когда-то было 32 зуба
+*/
+
+public class Solution0205 {
+
+	public static void main(String[] args) {
+		int toothCounter = 32;
+	    toothCounter--;
+	    toothCounter--;
+	    toothCounter--;
+	    toothCounter--;
+	    toothCounter--;      
+	    toothCounter--;
+	    toothCounter--;
+	    toothCounter--;
+	    toothCounter--;
+	    System.out.println(toothCounter);
+	}
+}
