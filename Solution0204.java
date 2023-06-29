744b72c59631a23d59b555f9ec095857de408521
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-06-29 21:32:41 +0300

    Create Solution0204

---
+++ Solution0204.java
@@ -0,0 +1,13 @@
+/*
+9-ка это перевернутая 6-ка
+*/
+public class Solution0204 {
+
+	public static void main(String[] args) {
+		int six = 6;
+		six++;
+		six++;
+		six++;
+		System.out.println(six);
+	}
+}
