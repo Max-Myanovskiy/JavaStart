497907a5f695156a08b109a79e85ce0547afa099
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-07-01 17:41:43 +0300

    Create Solution0207

---
+++ Solution0207.java
@@ -0,0 +1,17 @@
+/*
+Логотип решает все
+*/
+
+public class Solution0207 {
+
+	public static void main(String[] args) {
+		String s1 = "Do";
+		String s2 = "It";
+		String s3 = "Just";
+		
+		String tagline = s3 + s1 + s2;
+		
+		System.out.println(tagline);
+	}
+
+}
