9d0a8182f9afdb592e44de2e53cc9f76e0ffc518
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-07-02 20:18:59 +0300

    Create Solution0209

---
+++ Solution0209.java
@@ -0,0 +1,15 @@
+/*
+Конкатенация строк и чисел
+*/
+
+public class Solution0209 {
+
+	public static void main(String[] args) {
+		int x = 2;
+		int y = 4;
+		int z = 0;
+		String digits = x + y + "" + z;
+		System.out.println(digits);
+	}
+
+}
