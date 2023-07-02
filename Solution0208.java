1a73d5da832f4f3751e74cd7b0c9bee1946b93d4
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-07-02 20:12:31 +0300

    Create Solution0208

---
+++ Solution0208.java
@@ -0,0 +1,14 @@
+/*
+Пустота и пробелы 
+*/
+
+public class Solution0208 {
+
+	public static void main(String[] args) {
+		String emptiness = "пустота";
+		String fullness = emptiness + " " + emptiness + " " + emptiness;
+		
+		System.out.println(fullness);
+	}
+
+}
