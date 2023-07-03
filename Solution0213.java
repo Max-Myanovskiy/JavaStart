c9a89c7d5f9e9c22e2b8ec8fbca368694440d558
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-07-03 22:14:11 +0300

    Update Solution0213

--- Solution0213.java
+++ Solution0213.java
@@ -4,7 +4,7 @@
 
 public class Solution0213 {
   
-    piblic static void main(String[] args) {
+    public static void main(String[] args) {
         String title = "Senior Lead Principal Software Engineer Data Architect";
         String degree = "In college, I Majored in Political Science and Minored in Religious Studies.";
         String career = "Experienced Team Leader with strong Organizational Skills and a Successful career in Management.";
