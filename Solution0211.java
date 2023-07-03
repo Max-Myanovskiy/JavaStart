44f7e105a12d9f1eb19203344c38593ba612115d
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-07-03 20:15:14 +0300

    Create Solution0211

---
+++ Solution0211.java
@@ -0,0 +1,15 @@
+/*
+Получение длины строки
+*/
+
+public class Solution0211 {
+
+	public static void main(String[] args) {
+		String emptyString = "";
+		
+		System.out.println(emptyString.length());
+		System.out.println("Gomu Gomu no Bazooka!".length());
+		System.out.println((emptyString + 2 + 2 + "22").length());
+	}
+
+}
