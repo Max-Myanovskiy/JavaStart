44ce727ec7073970fa0e46b137ca69a9e078435b
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-07-05 18:42:39 +0300

    Update Solution0205

--- Solution0205.java
+++ Solution0205.java
@@ -2,19 +2,21 @@
 Когда-то было 32 зуба
 */
 
-public class Solution0205 {
-
-	public static void main(String[] args) {
+public class Solution0205 
+{
+	public static void main(String[] args) 
+	{
 		int toothCounter = 32;
-	    	toothCounter--;
-	    	toothCounter--;
-	    	toothCounter--;
-	    	toothCounter--;
-	    	toothCounter--;      
-	    	toothCounter--;
-	    	toothCounter--;
-	    	toothCounter--;
-	    	toothCounter--;
-	    	System.out.println(toothCounter);
+	    toothCounter--;
+	    toothCounter--;
+	    toothCounter--;
+	    toothCounter--;
+	    toothCounter--;      
+	    toothCounter--;
+	    toothCounter--;
+	    toothCounter--;
+	    toothCounter--;
+	    
+	    System.out.println(toothCounter);
 	}
 }
