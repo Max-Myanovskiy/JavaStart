50c7c40a1a99456722e738f858d882b2e5e3fd31
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-07-05 18:17:12 +0300

    Create Solution0212

---
+++ Solution0212.java
@@ -0,0 +1,15 @@
+/*
+Попрошу регистр на меня не повышать!
+*/
+
+public class Solution0212 
+{
+	public static void main(String[] args) 
+	{
+		String caps = "if I type in caps ";
+		String usa = "сша";
+		
+		System.out.println(usa.toUpperCase());
+		System.out.println((caps + "they know I mean business").toUpperCase());
+	}
+}
