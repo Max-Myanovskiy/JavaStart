14bbe24cb825a06945882f6115783516eacd8ef7
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-07-05 18:50:34 +0300

    Update Solution0206

--- Solution0206.java
+++ Solution0206.java
@@ -1,10 +1,12 @@
-public class Solution0206 {
-
-	public static void main(String[] args) {
-		String word = "Никто не скажет, верно ты поступил или нет, пока не столкнешься с последствиями.";
+public class Solution0206 
+{
+	public static void main(String[] args) 
+	{
+			String word = "Никто не скажет, верно ты поступил или нет, пока не столкнешься с последствиями.";
         	String phrase = "Когда людей окутывает тьма, они начинают видеть свет.";
         	String line = "Тому, кто сражается с монстрами, стоит следить, чтобы он сам не стал монстром.";
         	String text = "Человеческие чувства предназначены для того, чтобы откликаться на чувства других людей.";
+        	
         	System.out.println(word);
         	System.out.println(phrase);
         	System.out.println(line);
