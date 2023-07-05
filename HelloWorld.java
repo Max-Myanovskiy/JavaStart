0d8b2314319f0bd489bd2eeff44e3ba9d7c390a0
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-07-05 19:18:58 +0300

    Update HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
-public class HelloWorld {
-
-	public static void main(String[] args) {
+public class HelloWorld 
+{
+	public static void main(String[] args)
+	{
 		System.out.println("Hello World !!!");
 		System.out.println("Ukraine");
 		System.out.println("Zhytomyr");
