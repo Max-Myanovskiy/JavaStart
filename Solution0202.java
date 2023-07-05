51b80a2f15b3da29407395513ede79ca1c544648
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-07-05 19:16:17 +0300

    Update Solution0202

--- Solution0202.java
+++ Solution0202.java
@@ -2,11 +2,13 @@
 Количество полных метров
 */
 
-public class Solution0202 {
-
-	public static void main(String[] args) {
-		 int centimeters = 243;
-		 int meters = centimeters/100;
-		 System.out.println(meters);
+public class Solution0202 
+{
+	public static void main(String[] args) 
+	{
+		int centimeters = 243;
+		int meters = centimeters/100;
+		
+		System.out.println(meters);
 	}
 }
