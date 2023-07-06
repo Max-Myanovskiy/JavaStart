cc964e91e9d57b3c215ca14ef32ec4c933d9f707
Max-Myanovskiy <myanovskiy.m@gmail.com>
2023-07-06 16:09:33 +0300

    Update Solution0210

--- Solution0210.java
+++ Solution0210.java
@@ -2,14 +2,14 @@
 С каждым разом огромнее
 */
 
-public class Solution0210 {
-
-	public static void main(String[] args) {
+public class Solution0210 
+{
+	public static void main(String[] args) 
+	{
 		String bigAmount = "500";
 		String greatAmount = "100000";
 		int hugeAmount = Integer.parseInt(bigAmount) + Integer.parseInt(greatAmount);
 		
 		System.out.println(hugeAmount);
 	}
-
 }
